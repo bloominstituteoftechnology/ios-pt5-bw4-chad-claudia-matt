5476fa520f581b86a91129dcf26d091c5e93cc60
Chad Parker <chad@chadparker.com>
2020-08-18 19:26:43 -0700

    Add Xcode project

---
+++ Transcribe/NoteController.swift
@@ -0,0 +1,20 @@
+//
+//  NoteController.swift
+//  Transcribe
+//
+//  Created by Chad Parker on 8/18/20.
+//  Copyright © 2020 Chad-Claudia-Matt. All rights reserved.
+//
+
+import Foundation
+
+class NoteController: ObservableObject {
+
+    var previewNotes: [Note] {
+        [
+            Note(title: "Test 1", bodyText: "Ajfkldsjfldksjfldskjfdkls jkflds jkflds jfklds jfklds", audioFilename: ""),
+            Note(title: "Test 2", bodyText: "Ajfkldsjfldksjfldskjfdkls jkflds jkflds jfklds jfklds", audioFilename: ""),
+            Note(title: "Test 3", bodyText: "Ajfkldsjfldksjfldskjfdkls jkflds jkflds jfklds jfklds", audioFilename: ""),
+        ]
+    }
+}
