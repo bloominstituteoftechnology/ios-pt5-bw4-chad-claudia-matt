db16d01f89548925e9ab3984830e6e3d7f928704
Matt Martindale <martindalematt18@yahoo.com>
2020-08-19 20:08:49 -0600

    Added CardView.swift

---
+++ Transcribe/SubViews/CardView.swift
@@ -0,0 +1,21 @@
+//
+//  CardView.swift
+//  Transcribe
+//
+//  Created by Matthew Martindale on 8/19/20.
+//  Copyright © 2020 Chad-Claudia-Matt. All rights reserved.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
