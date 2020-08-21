f6ee10148b9b39620224b4ba766cd047ad506d6f
Claudia Maciel <clc_80@yahoo.com>
2020-08-20 18:46:46 -0700

    added editText View

---
+++ Transcribe/MainViews/EditText.swift
@@ -0,0 +1,21 @@
+//
+//  EditText.swift
+//  Transcribe
+//
+//  Created by Claudia Maciel on 8/20/20.
+//  Copyright © 2020 Chad-Claudia-Matt. All rights reserved.
+//
+
+import SwiftUI
+
+struct EditText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditText_Previews: PreviewProvider {
+    static var previews: some View {
+        EditText()
+    }
+}
