de1196dd74641e46ef6a935645b38272460b804d
Claudia Maciel <clc_80@yahoo.com>
2020-08-21 19:43:25 -0700

    Changes persist after being changed.

--- Transcribe/ModelControllers/NoteController.swift
+++ Transcribe/ModelControllers/NoteController.swift
@@ -10,11 +10,17 @@ import Foundation
 
 class NoteController: ObservableObject {
 
-    var previewNotes: [Note] {
-        [
-//            Note(title: "Test 1", bodyText: "Ajfkldsjfldksjfldskjfdkls jkflds jkflds jfklds jfklds", audioFilename: ""),
-//            Note(title: "Test 2", bodyText: "Ajfkldsjfldksjfldskjfdkls jkflds jkflds jfklds jfklds", audioFilename: ""),
-//            Note(title: "Test 3", bodyText: "Ajfkldsjfldksjfldskjfdkls jkflds jkflds jfklds jfklds", audioFilename: ""),
-        ]
+    @Published var previewNotes: [Note] = [
+        Note(title: "Test 1", bodyText: "Ajfkldsjfldksjfldskjfdkls jkflds jkflds jfklds jfklds", audioFilename: "", color: .red),
+        Note(title: "Test 2", bodyText: "Ajfkldsjfldksjfldskjfdkls jkflds jkflds jfklds jfklds", audioFilename: "", color: .orange),
+        Note(title: "Test 3", bodyText: "Ajfkldsjfldksjfldskjfdkls jkflds jkflds jfklds jfklds", audioFilename: "", color: .blue)
+    ]
+    
+    func updateMessage(for note: Note, to newMessage: String) {
+        if let index = previewNotes.firstIndex(where: { $0.id == note.id }) {
+            previewNotes[index].bodyText = newMessage
+        }
+
     }
+
 }
