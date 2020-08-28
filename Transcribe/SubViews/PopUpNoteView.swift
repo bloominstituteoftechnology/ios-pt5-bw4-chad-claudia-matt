ca69580d0f26af220d9b852c8e179bf3744687a5
Matt Martindale <martindalematt18@yahoo.com>
2020-08-27 21:18:19 -0600

    User can now save edited title and text

--- Transcribe/SubViews/PopUpNoteView.swift
+++ Transcribe/SubViews/PopUpNoteView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 
 struct PopUpNoteView: View {
     
+    @EnvironmentObject var noteController: NoteController
+    
     var note: Note
     @State var editedTitle: String = ""
     @State var editedBodyText: String = ""
@@ -21,14 +23,34 @@ struct PopUpNoteView: View {
                 .padding(10)
                 .font(Font.system(size: 25))
                 .foregroundColor(.white)
+                .fixedSize(horizontal: false, vertical: true)
             
             Divider()
             
-            ScrollView(.vertical, showsIndicators: false) {
-                TextField("\(note.bodyText)", text: $editedBodyText)
-                    .foregroundColor(Color(#colorLiteral(red: 0.8967368603, green: 0.8914062381, blue: 0.9008343816, alpha: 1)))
-            }.padding(.horizontal, 20)
-                .padding(.bottom, 20)
+            VStack {
+                ScrollView(.vertical, showsIndicators: false) {
+                    TextField("\(note.bodyText)", text: $editedBodyText)
+                        .foregroundColor(Color(#colorLiteral(red: 0.8967368603, green: 0.8914062381, blue: 0.9008343816, alpha: 1)))
+                }.padding(.horizontal, 20)
+                    .padding(.bottom, 20)
+                
+                Spacer()
+                Divider()
+                HStack{
+                    Spacer()
+                    Button(action: {
+                        self.noteController.updateTitle(for: self.note, to: self.editedTitle)
+                        self.noteController.updateMessage(for: self.note, to: self.editedBodyText)
+                        withAnimation {
+                            self.noteController.showPopUp = false
+                        }
+                    }) {
+                        Text("Save")
+                        .padding()
+                            .foregroundColor(Color("cardColor3"))
+                    }
+                }
+            }
         }.frame(width: UIScreen.main.bounds.width - 80, height: UIScreen.main.bounds.height * 0.5)
             .background(RadialGradient(gradient: Gradient(colors: [Color("cardColor2"), Color("cardColor1")]), center: UnitPoint(x: 0, y: 0), startRadius: 0, endRadius: UIScreen.main.bounds.width * 2))
         .cornerRadius(20)
@@ -44,7 +66,7 @@ struct PopUpNoteView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             PopUpNoteView(note: Note(title: "Popup", bodyText: "body text", audioFilename: "", category: "popUp")).environmentObject(NoteController())
-            PopUpNoteView(note: Note(title: "Popup", bodyText: "body text", audioFilename: "", category: "popUp")).environment(\.colorScheme, .dark)
+            PopUpNoteView(note: Note(title: "Popup", bodyText: "body text", audioFilename: "", category: "popUp")).environmentObject(NoteController()).environment(\.colorScheme, .dark)
         }
     }
 }
