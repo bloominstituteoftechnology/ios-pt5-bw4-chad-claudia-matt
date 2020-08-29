b92ee664aad0819675656335cbf68f0b60dd19cd
Chad Parker <chad@chadparker.com>
2020-08-28 20:06:42 -0700

    Reset SceneDelegate back to ContentView

--- Transcribe/SceneDelegate.swift
+++ Transcribe/SceneDelegate.swift
@@ -20,9 +20,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        //let contentView = ContentView()
+        let contentView = ContentView()
         //let contentView = RecordView()
-        let contentView = DetailView(note: noteWithRecordings)
+//        let contentView = DetailView(note: noteWithRecordings)
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
