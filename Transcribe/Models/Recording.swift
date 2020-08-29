86257e5a04b8af0751f38fe758acf89f343be4e3
Chad Parker <chad@chadparker.com>
2020-08-28 20:06:14 -0700

    Make Recording a class, but this isn't working right either

--- Transcribe/Models/Recording.swift
+++ Transcribe/Models/Recording.swift
@@ -8,9 +8,36 @@
 
 import Foundation
 
-struct Recording: Identifiable, Hashable, Codable {
+class Recording: Identifiable, Hashable, Codable {
+
     let id = UUID()
     let audioFileURL: URL
-    let textTranscript: String
+    var textTranscript: String?
     let duration: Int
+
+    init(audioFileURL: URL, textTranscript: String? = nil, duration: Int) {
+        self.audioFileURL = audioFileURL
+        self.textTranscript = textTranscript
+        self.duration = duration
+
+        if textTranscript == nil {
+            transcribe()
+        }
+    }
+
+    private func transcribe() {
+        Transcriber.transcribeAudioURL(audioFileURL) { text in
+            self.textTranscript = text
+        }
+    }
+
+    // MARK: - Hashable
+
+    static func == (lhs: Recording, rhs: Recording) -> Bool {
+        lhs.id == rhs.id
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(self.id)
+    }
 }
